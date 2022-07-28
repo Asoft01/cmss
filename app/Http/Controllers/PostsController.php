c414cd59f0cd6bdda65719f623341ffc27a87871
Adeleke Hammed <lekhad19@gmail.com>
2022-07-28 18:05:16 +0100

    Saving Posts in the database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Posts\CreatePostsRequest;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -32,9 +34,24 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostsRequest $request)
     {
-        //
+        // Upload the image to storage 
+        // dd($request->image);
+        // dd($request->image->store('posts'));
+        $image = $request->image->store('posts');
+        // create the post 
+        Post::create([
+            'title' => $request->title, 
+            'description' => $request->description,
+            'content' => $request->content, 
+            'image' => $image
+        ]);
+        // flash message 
+        session()->flash('success', 'Post created successfully');
+        // redirect the user 
+        return redirect(route('posts.index'));
+
     }
 
     /**
