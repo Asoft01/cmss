9b793daeb414cff6cf7932aa929a692dc7fe0650
Adeleke Hammed <lekhad19@gmail.com>
2022-07-28 18:39:16 +0100

    Disiplaying Posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,27 @@
  <div class="card card-default">
     <div class="card-header">
         <div class="card-body">
-
+            <table class="table">
+                <thead>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td><img src="{{ asset($post->image) }}" width="120px" height="60px" alt="Hello"></td>
+                            <td>{{ $post->title }}</td>
+                            <td>
+                                <a href="" class="btn btn-info sm">Edit</a>
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-danger sm">Trash</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
  </div>
